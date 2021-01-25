4b323034e7d2b5508ca38cfdda17f3496e622c00
Christian Berentsen <chrberen@cisco.com>
2021-01-25 16:58:21 +0100

    Fix dead code warnings

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod language;
+pub mod ga;
+
+pub mod ring;
+pub mod group;
+pub mod field;
+pub mod abelian;
+pub mod monoid;
+
+pub use crate::ga::*;
+pub use crate::language::*;
+pub use crate::monoid::*;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-mod language;
-mod monoid;
-mod group;
-mod abelian;
-mod ring;
-mod field;
-mod ga;
-
 fn main() {
     //! Based on egg tutorial code https://docs.rs/egg/0.6.0/egg/tutorials/_02_getting_started/index.html
     println!("Run cargo test instead");
